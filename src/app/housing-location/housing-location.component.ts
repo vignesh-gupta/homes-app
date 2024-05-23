bca06fa3da1b73cdc5998b159687efd357e3ec09
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-05-23 15:35:22 +0530

    add directives & get inputs

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="External photos of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
   `,
-  styleUrl: './housing-location.component.css'
+  styleUrl: './housing-location.component.css',
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
